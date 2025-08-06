53a4f45822448d809788160d00e475c26c08ac30
Vincent Mahnke <accounts@vimaster.de>
2025-08-06 23:27:25 +0200

    chore: Relocates infrastructure terrform definition

--- infrastructure/subdomain.tf
+++ infrastructure/subdomain.tf
@@ -1,25 +1,16 @@
-terraform {
-  required_providers {
-    allinkl = {
-      source = "ViMaSter/allinkl"
-      version = "0.1.0"
-    }
-  }
-}
-
-provider "allinkl" {
-    kas_auth_type = "plain"
-}
-
 resource "random_password" "ddns_password" {
-    length  = 30
-    special = false
+  length  = 30
+  special = false
 }
 
-resource "allinkl_ddns" "uptime" {
-    dyndns_comment   = "Uptime Kuma"
-    dyndns_password  = random_password.ddns_password.result
-    dyndns_zone      = "mahn.ke"
-    dyndns_label     = "uptime.by.vincent"
-    dyndns_target_ip = "88.99.215.101"
+locals {
+  subdomain_label = replace(replace(basename(dirname(path.cwd)), "-by-vincent", ""), "-", ".")
+}
+
+resource "allinkl_ddns" "subdomain" {
+  dyndns_comment   = local.subdomain_label
+  dyndns_password  = random_password.ddns_password.result
+  dyndns_zone      = "mahn.ke"
+  dyndns_label     = "${local.subdomain_label}.by.vincent"
+  dyndns_target_ip = "88.99.215.101"
 }
